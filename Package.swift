a503e15b374118371453f8d08a760b5302a5934c
CaffeinePower <86540016+cffnpwr@users.noreply.github.com>
2026-03-11 14:52:29 +0900

    chore(deps): :package: pin swift argument parser v1.6.2 (#17)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "darwin-ism",
   platforms: [.macOS(.v14)],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", "1.3.0" ..< "1.7.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.6.2"),
   ],
   targets: [
     .executableTarget(
